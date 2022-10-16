24a0a1209acd518d1ea6bbbcac61cbfcbd9c8416
PrismaticGrenade <107100962+PrismaticGrenade@users.noreply.github.com>
2022-10-16 12:48:19 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login Form</title>
+
+    <link rel="stylesheet" href="css/bootstrap.min.css" />
+</head>
+
+<body class="bg-light">
+    <header>
+        <div class="jumbotron jumbotron-fluid">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8">
+                        <h1>Selamat Datang di Binus University</h1>
+                        <p>Bergabunglah Bersama kami bagian dari Binus University</p>
+                    </div>
+                    <div class="col-md-4">
+                        <a href="login.php" class="btn btn-secondary">Masuk</a>
+                        <a href="register.php" class="btn btn-primary">Daftar</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <section>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <img class="img img-responsive" src="img/logo-binus-university resized 500x300.png" />
+                </div>
+            </div>
+        </div>
+    </section>
+
+</body>
+
+</html>
